721ccd83b8bded726ee1f9175adb52c5be01a336
juan cruz <juan.feuilles@gmail.com>
2020-11-17 14:30:20 -0200

    Update lista.c

--- lista.c
+++ lista.c
@@ -22,15 +22,15 @@ void crear_lista(tLista * l) {
  L' = A,B,E,C,D
 **/
 void l_insertar(tLista l, tPosicion p, tElemento e) {
-    
-	if (p==NULL)//verifico que sea un posición válida 
+
+	if (p==NULL)//verifico que sea un posición válida
         exit(LST_POSICION_INVALIDA);
-	
+
     tPosicion posNuevaCelda = (tPosicion) malloc(sizeof(struct celda));//reservo memoria
-    
+
 	if (posNuevaCelda == NULL)//verifico que se haya creado bien
         exit(LST_ERROR_MEMORIA);
-	
+
 	//seto los parametros, la referencia al siguiente y del anterior a el
     posNuevaCelda->elemento = e;
     posNuevaCelda->siguiente = p->siguiente;
@@ -59,17 +59,16 @@ void l_eliminar(tLista l, tPosicion p, void (*fEliminar)(tElemento)) {
 /**
  Destruye la lista L, elimininando cada una de sus posiciones. Los elementos almacenados en las posiciones son eliminados mediante la función fEliminar parametrizada.
 **/
-void l_destruir(tLista * l, void (*fEliminar)(tElemento)) {
+void l_destruir(tLista * l, void (*fEliminar)(tElemento)){
     tPosicion pos = *l;
     tPosicion aux = pos;
 
-	
+
 	//recorre la lista eliminando posición por posicion y tambien el contenido de las misma
-    while(pos->siguiente!= NULL) { 
+    while(pos->siguiente!= NULL){
         aux = aux->siguiente;
         fEliminar(pos->elemento);
         free(pos);
-        printf("1");
         pos = aux;
     }
     *l = NULL;
@@ -99,28 +98,35 @@ tPosicion l_primera(tLista l){
  Si P es fin(L), finaliza indicando LST_NO_EXISTE_SIGUIENTE.
 **/
 tPosicion l_siguiente(tLista l, tPosicion p) {
+
+    //chequeo que la posicion sean válida
     if(p==NULL)
         exit(LST_POSICION_INVALIDA);
+
+    //no podes pedir siguiente a a fin
     if(p==l_fin(l))
         exit(LST_NO_EXISTE_SIGUIENTE);
-    return p->siguiente;
-}
+
+    return p->siguiente;}
 
 /**
  Recupera y retorna la posición anterior a P en L.
  Si P es primera(L), finaliza indicando LST_NO_EXISTE_ANTERIOR.
 **/
 tPosicion l_anterior(tLista l, tPosicion p) {
+
     if(p==NULL)
         exit(LST_POSICION_INVALIDA);
+
     if(p==l_primera(l))
         exit(LST_NO_EXISTE_ANTERIOR);
 
     tPosicion pos = l;
+
     while(pos->siguiente != p)
-        pos = pos->siguiente;
-    return pos;
-}
+         pos = pos->siguiente;
+
+return pos;}
 
 /**
  Recupera y retorna la última posición de L.
@@ -129,7 +135,7 @@ tPosicion l_anterior(tLista l, tPosicion p) {
 tPosicion l_ultima(tLista l) {
     tPosicion pos = l;
 
-    if(l->siguiente!=NULL)  //la lista esta vacia, primera = ultima = fin
+    if(l->siguiente!=NULL)//la lista esta vacia, primera = ultima = fin
 
         while(((pos->siguiente)->siguiente) != NULL)
               pos = pos->siguiente;
@@ -142,32 +148,28 @@ tPosicion l_ultima(tLista l) {
  Si L es vacía, primera(L) = ultima(L) = fin(L).
 **/
 tPosicion l_fin(tLista l) {
-    tPosicion pos = l;
-    if(l->siguiente==NULL) { //la lista esta vacia, primera = ultima = fin
-        return l;
-    } else {
-        while(pos->siguiente!=NULL)
-            pos = pos->siguiente;
-    }
-    return pos;
-}
+      tPosicion pos = l;
+
+      while(pos->siguiente!=NULL)
+            pos = pos->siguiente;//recorro las poasdiciones te tienen un elemento
+
+return pos;}
 
 /**
  Metodo que devuelve la longitud de la lista lista.
 **/
 int l_longitud(tLista l) {
+
     int contador = 0;
-    if(l_primera(l) == l_fin(l)) //si no tiene elementos
-        return contador;
-    else {
-        tPosicion pos = l->siguiente;
-        while(pos != NULL) {
+
+    if(l_primera(l) != l_fin(l)) //si no tiene elementos
+
+       {tPosicion pos = l->siguiente;
+        while(pos != NULL){
             pos = pos->siguiente;
-            contador++;
-        }
-    }
-    return contador;
-}
+            contador++;}
+            }
+return contador;}
 
 
 
